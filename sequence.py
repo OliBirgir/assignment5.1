cf76760d86f89b219177415baeacad8bcf8d854d
Oli Birgir <Obirgir@gmail.com>
2019-09-05 10:35:48 +0000

    added more to the algorithm and finished coding

--- sequence.py
+++ sequence.py
@@ -5,5 +5,20 @@
 # you will need few integers. a_int, b_int and c_int, sum and a counter, which counts the input
 # while the input is more or the same as the counter, it should summarize a_int, b_int and c_int.
 #print out the sum and add 1 to the counter
-
+#While a_int is more or the same as two, the number c will be the same as b, which leads to correct printing for 3
+#then b_int becomes a_int and the a_int becomes the sum.
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+a_int = 1
+b_int = 0
+c_int = 0
+counter = 0
+sum_int = 0
+while n>counter:
+    sum_int = (a_int + b_int + c_int)
+    print(sum_int)
+    counter+=1
+    if a_int >= 2:
+        c_int = b_int
+    b_int = a_int
+    a_int = sum_int
 
