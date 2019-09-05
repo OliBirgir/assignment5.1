f34d7dd7eb92df5c819f28ebb1766d56b238e29b
Oli Birgir <Obirgir@gmail.com>
2019-09-05 10:31:54 +0000

    added more to the algorithm

--- sequence.py
+++ sequence.py
@@ -2,6 +2,8 @@
 #user tells us the length of the sequence, which means that we will print out x amount of numbers in a sequence.
 #the code is supposed to print out all three numbers below.
 # for example: 1,2,3,(3+2+1) = 6, (6+3+2) = 11....
-
+# you will need few integers. a_int, b_int and c_int, sum and a counter, which counts the input
+# while the input is more or the same as the counter, it should summarize a_int, b_int and c_int.
+#print out the sum and add 1 to the counter
 
 
