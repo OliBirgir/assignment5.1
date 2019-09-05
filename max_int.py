71872d75a2c09024d3f42966234541324df14273
Oli Birgir <Obirgir@gmail.com>
2019-09-05 09:31:17 +0000

    Added an algorithm

---
+++ max_int.py
@@ -0,0 +1,4 @@
+#1Create a line that represents the max integer
+#A user inputs an positive integer.
+#The algorithm checks if the number you inputed is higher then the max integer.
+#If it's higher, the max integer becomes the same number that the user inputted, since it was higher than the max.
